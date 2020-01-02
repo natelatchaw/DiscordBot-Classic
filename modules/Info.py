64f69df25f3446e94cbfc7253081433318752468
Nate Latchaw <natelatchaw@gmail.com>
2020-01-02 13:26:00 -0600

    Minor tweaks

--- modules/Info.py
+++ modules/Info.py
@@ -5,14 +5,13 @@ class Info():
     def __init__(self, args):
         self.name = 'Info'
         self.description = 'Returns info about the platform'
-        self.syntax = ['Info']
+        self.syntax = ['info']
 
     async def run(self):
         self.icon_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/200px-Python-logo-notext.svg.png'
 
         embed = discord.Embed()
         embed.title = 'Platform Information'
-        # embed.description = 'Underlying platform information'
         embed.add_field(name = '**System**', value = platform.system(), inline = False)
         embed.add_field(name = '**Node**', value = platform.node(), inline = False)
         embed.add_field(name = '**Release**', value = platform.release(), inline = False)
