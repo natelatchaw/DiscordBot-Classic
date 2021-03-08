ca3a5b30b0a9f8e6ce750292e9c41fa807b972e8
Nathan Latchaw <natelatchaw@gmail.com>
2021-03-08 17:37:37 -0500

    tweaked run script and updated gitignore

--- run.py
+++ run.py
@@ -1,37 +1,51 @@
+import asyncio
 import discord
 import discord.ext
 from bot.core import Core
 from bot.handler import Handler
 from bot.archiver import Archiver
 
-# initialize client object
-client = discord.Client(intents=discord.Intents.all())
-
-# initialize Core
-main = Core()
-
-# initialize Handler
-handler = Handler(client, main._uxStore)
-
-@client.event
-async def on_ready():
-    # startup tasks
-    print(f'{client.user.name} loaded in {main.mode} mode.')
-
-@client.event
-async def on_message(message):
-    # filter non-message objects
-    if not isinstance(message, discord.Message):
-        raise TypeError('Received an object that is not a message.')
-    # if config is missing prefix
-    elif not main.prefix:
-        raise ValueError('No prefix has been set.')
-    # handle message
-    await handler.process(message)
-
 try:
-    client.run(main.token)
-except ValueError as valueError:
-    print(valueError)
-except TypeError as typeError:
-    print(typeError)
+    # get the event loop
+    loop = asyncio.get_event_loop()
+
+    # initialize client object
+    client = discord.Client(intents=discord.Intents.all())
+    # initialize Core
+    main = Core()
+    # initialize Handler
+    handler = Handler(client, main)
+
+    @client.event
+    async def on_ready():
+        # startup tasks
+        print(f'{client.user.name} loaded in {main.mode} mode.')
+
+    @client.event
+    async def on_message(message):
+        # filter non-message objects
+        if not isinstance(message, discord.Message):
+            raise TypeError('Received an object that is not a message.')
+        # handle message
+        await handler.process(message)
+
+    # try to start the bot client
+    loop.run_until_complete(client.start(main.token))
+# if TypeError or ValueError occurs
+except (TypeError, ValueError) as error:
+    print(error)
+    raise
+# if program is interrupted in console
+except KeyboardInterrupt as keyboardInterrupt:
+    print(f'KeyboardInterrupt event occurred: {keyboardInterrupt}')
+# if the client fails to login
+except discord.errors.LoginFailure as loginFailure:
+    print(f'Login failure occurred: {loginFailure}')
+# if an unexpected error occurs
+except:
+    # throw it
+    raise
+# terminate gracefully
+finally:
+    loop.run_until_complete(client.logout())
+    loop.run_until_complete(client.close())
