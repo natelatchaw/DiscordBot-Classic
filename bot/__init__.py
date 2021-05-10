257d616ae50f246f07901931ae08c784aa8daab2
Nathan Latchaw <natelatchaw@gmail.com>
2021-05-10 09:41:03 -0500

    setup.py

---
+++ bot/__init__.py
@@ -0,0 +1,8 @@
+"""
+bot
+
+A module-based message handler.
+"""
+
+__version__ = '0.0.2'
+__author__ = 'Nathan Latchaw'
---
+++ bot/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='bot',
+    version='0.0.2',
+    description='A module-extensible message bot',
+    author='Nathan Latchaw',
+    author_email='natelatchaw@gmail.com',
+    license='Creative Commons',
+    packages=['bot'],
+    url='not available',
+    install_requires=[
+        'discord.py',
+    ],
+    classifiers=[
+        'Development Status :: Alpha',
+        'Intended Audience :: Hobbyist',
+        'Operating System :: Agnostic',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
