0a9315d9e40b79484d4d03ac24fba0d1d910cc60
Nathan Latchaw <natelatchaw@gmail.com>
2021-05-10 11:16:57 -0500

    updated to match class changes

--- cmd.py
+++ cmd.py
@@ -6,14 +6,14 @@ from bot.handler import Handler
 loop = asyncio.get_event_loop()
 
 core = Core()
-
-handler = Handler(core)
+handler = Handler()
+handler.load(core.modules)
 
 async def await_input():
     while True:
         message: str = input()
         try:
-            await handler.process(message)
+            await handler.process(core.prefix, message)
         except Exception as exception:
             print(exception)
 
