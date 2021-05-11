334beefbdcdee4479c0619af6ea0e02388743324
Nathan Latchaw <natelatchaw@gmail.com>
2021-05-11 12:47:38 -0500

    changed classes to reference refactored module

--- run.py
+++ run.py
@@ -2,31 +2,31 @@ import asyncio
 from datetime import datetime
 import discord
 import discord.ext
-from bot.core import Core
-from bot.handler import Handler
-from bot.archiver import Archiver
-from bot.logger import Logger
+from router.settings import Settings
+from router.handler import Handler
+from router.logger import Logger
 
-async def print_login_message(core: Core):
+async def print_login_message(settings: Settings):
     if client.user:
         startup_message = f'Bot client {client.user.mention} initialized in {round((datetime.now() - instantiated_time).total_seconds(), 1)}s'
         for guild in client.guilds:
-            logger: Logger = Logger(core, guild)
+            logger: Logger = Logger(settings, guild)
             await logger.print(startup_message)
 
-async def print_logout_message(core: Core):
+async def print_logout_message(settings: Settings):
     if client.user:
         shutdown_message = f'Bot client {client.user.mention} shutting down. Runtime: {round((datetime.now() - instantiated_time).total_seconds(), 1)}s'
         for guild in client.guilds:
-            logger: Logger = Logger(core, guild)
+            logger: Logger = Logger(settings, guild)
             await logger.print(shutdown_message)
 
 async def archive_message(message: discord.Message):
-    archiver: Archiver = Archiver(message.channel)
+    ###archiver: Archiver = Archiver(message.channel)
     # create a table for the current channel if it hasn't been created yet
-    await archiver.create()
+    ###await archiver.create()
     # insert the current message into the archiver
-    await archiver.insert(message)
+    ###await archiver.insert(message)
+    pass
 
 try:
     # get the time the bot was initialized
@@ -36,17 +36,17 @@ try:
     # initialize client object
     client = discord.Client(intents=discord.Intents.all())
     # initialize Core
-    core = Core()
+    settings = Settings()
     # initialize Handler
     handler = Handler()
     # load modules folder
-    handler.load(core.modules)
+    handler.load(settings.modules)
 
     @client.event
     async def on_ready():
         # startup tasks
-        print(f'{client.user.name} loaded in {core.mode} mode.')
-        await print_login_message(core)
+        print(f'{client.user.name} loaded in {settings.mode} mode.')
+        await print_login_message(settings)
 
     @client.event
     async def on_message(message):
@@ -56,14 +56,14 @@ try:
         # initialize optionals to pass to handler
         optionals: dict = {
             '_message': message,
-            '_core': core,
-            '_archiver': Archiver(message.channel),
-            '_logger': Logger(core, message.guild),
+            '_core': settings,
+            ###'_archiver': Archiver(message.channel),
+            '_logger': Logger(settings, message.guild),
             '_modules': handler._modules
         }
         try:
             # handle message
-            await handler.process(core.prefix, message.content, optionals=optionals)
+            await handler.process(settings.prefix, message.content, optionals=optionals)
         except ValueError:
             # no prefix configured
             pass
@@ -74,7 +74,7 @@ try:
             # archive message
             await archive_message(message)
     # try to start the bot client
-    loop.run_until_complete(client.start(core.token))
+    loop.run_until_complete(client.start(settings.token))
 # if TypeError or ValueError occurs
 except (TypeError, ValueError) as error:
     print(error)
@@ -91,5 +91,5 @@ except:
 # terminate gracefully
 finally:
     # shutdown tasks
-    loop.run_until_complete(print_logout_message(core))
+    loop.run_until_complete(print_logout_message(settings))
     loop.run_until_complete(client.close())
