3f6f55f1578e7fffeb5980239ba5e5f4e6d51fb9
Nate <natelatchaw@gmail.com>
2022-06-15 12:29:36 -0500

    improved error message formatting

--- commandHandler.py
+++ commandHandler.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 import logging
 import re
 import traceback
@@ -5,7 +6,7 @@ from logging import Logger
 from re import Match
 from typing import Any, List, Optional
 
-from discord import Message
+from discord import Embed, Message
 from router import Handler, HandlerError
 
 from context import Context
@@ -20,10 +21,26 @@ class CommandHandler(Handler):
         self._limiter: Optional[RateLimiter] = None
         super().__init__(parameter_prefix)
 
+    def __build_error_message__(self, error: Exception, use_verbose: bool = False) -> Embed:
+        embed: Embed = Embed()
+
+        embed.title = f'{type(error).__name__}'
+        embed.description = f'An error occurred during command execution.'
+        embed.add_field(name='Details', value=str(error), inline=False)
+        embed.timestamp = datetime.now(tz=timezone.utc)
+
+        if use_verbose:
+            trace: str = ''.join(traceback.format_tb(error.__traceback__))
+            embed.add_field(name='Traceback', value=trace, inline=False)
+        
+        return embed
+
+
     def addLimiter(self, limiter: RateLimiter) -> None:
         self._limiter = limiter
 
     async def handle(self, prefix: str, message: Message, *, context: Context):
+        
         # create args list from context instance
         args: List[Any] = [context]
         # get command from message
@@ -33,7 +50,6 @@ class CommandHandler(Handler):
         # remove prefix from message content
         command_message: str = re.sub(rf"^{prefix}", "", message.content)
 
-
         try:
             # ratelimit check the message if available
             if self._limiter: self._limiter.check(message)
@@ -41,13 +57,11 @@ class CommandHandler(Handler):
             await super().process(command_message, args=args)
         except Exception as error:
             log.error(error)
+            log.error(''.join(traceback.format_tb(error.__traceback__)))
 
-            response: str = f'An error occurred during command execution'
-            if True:
-                response = response + f":```{error}```"
-            if context.settings.ux.verbose:
-                response = response + f"```{''.join(traceback.format_tb(error.__traceback__))}```"
-            await message.reply(response)
+            embed: Embed = self.__build_error_message__(error, context.settings.ux.verbose)
+            embed.set_author(name=context.client.user.name, icon_url=context.client.user.avatar_url)
+            await message.reply(embed=embed)
 
 
 class MissingPrefixError(HandlerError):
