185245c8e4f03a42a389e89b42cab688b839fb4a
Nathan Latchaw <natelatchaw@gmail.com>
2022-06-24 23:39:09 -0500

    fixed traceback message size issue

--- commandHandler.py
+++ commandHandler.py
@@ -1,10 +1,11 @@
 import logging
 import re
+import textwrap
 import traceback
 from datetime import datetime, timezone
 from logging import Logger
 from re import Match
-from typing import Any, List, Optional
+from typing import Any, List, Literal, Optional
 
 from discord import Embed, Message
 from router import Handler, HandlerError
@@ -14,6 +15,12 @@ from rateLimiter import RateLimiter
 
 log: Logger = logging.getLogger(__name__)
 
+MAX_MESSAGE_SIZE: Literal[2000] = 2000
+"""
+The maximum amount of characters
+permitted in a Discord message
+"""
+
 
 class CommandHandler(Handler):
 
@@ -21,17 +28,13 @@ class CommandHandler(Handler):
         self._limiter: Optional[RateLimiter] = None
         super().__init__(parameter_prefix)
 
-    def __build_error_message__(self, error: Exception, use_verbose: bool = False) -> Embed:
+    def __build_error_message__(self, error: Exception) -> Embed:
         embed: Embed = Embed()
 
         embed.title = f'{type(error).__name__}'
         embed.description = f'An error occurred during command execution.'
         embed.add_field(name='Details', value=str(error), inline=False)
         embed.timestamp = datetime.now(tz=timezone.utc)
-
-        if use_verbose:
-            trace: str = ''.join(traceback.format_tb(error.__traceback__))
-            embed.add_field(name='Traceback', value=trace, inline=False)
         
         return embed
 
@@ -64,9 +67,33 @@ class CommandHandler(Handler):
             log.error(error)
             log.error(''.join(traceback.format_tb(error.__traceback__)))
 
-            embed: Embed = self.__build_error_message__(error, context.settings.ux.verbose)
+            embed: Embed = self.__build_error_message__(error)
             embed.set_author(name=context.client.user.name, icon_url=str(context.client.user.avatar_url))
             await message.reply(embed=embed)
+
+            if context.settings.ux.verbose: await self.__print_traceback__(context, error)
+
+    async def __print_traceback__(self, context: Context, error: Exception) -> None:
+        """
+        Assembles and splits the traceback to message lengths
+        compatible with a standard Discord message.
+        """
+        # assemble the trace
+        trace: str = ''.join(traceback.format_tb(error.__traceback__))
+        # define the block tag for code block messages
+        block_tag: str = '```'
+        # calculate the max characters that can be inserted into each message's code block
+        max_characters: int = MAX_MESSAGE_SIZE - (2 * len(block_tag))
+        # break the response into string segments with length equivalent to the maximum character limit
+        segments: List[str] = textwrap.wrap(trace, max_characters, break_long_words=False, replace_whitespace=False)
+        # set the initial message reference to the original message
+        message: Message = context.message
+        # for each segment# for each segment
+        for segment in segments:
+            # send the message and set the message reference to the sent message
+            message = await message.reply(f'{block_tag}\n{segment}\n{block_tag}')
+
+
 
 
 class MissingPrefixError(HandlerError):
