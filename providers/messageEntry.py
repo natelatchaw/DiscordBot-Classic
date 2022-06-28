fc1692929b5f1fd0648600e61a9903d678e2b626
Nathan Latchaw <natelatchaw@gmail.com>
2022-06-27 23:51:14 -0500

    Add Storable conformance

--- providers/messageEntry.py
+++ providers/messageEntry.py
@@ -2,9 +2,13 @@ from __future__ import annotations
 
 from datetime import datetime
 from sqlite3 import Row
-from typing import List
+from typing import Any, List, Tuple, Type
 
 from discord import Attachment, Message
+from database.column import ColumnBuilder
+
+from database.storable import Storable, TStorable
+from database.table import Table, TableBuilder
 
 
 class AttachmentEntry():
@@ -30,11 +34,7 @@ class AttachmentEntry():
         return self._url
         
 
-class MessageEntry():
-
-    @classmethod
-    def fromMessage(cls, message: Message) -> MessageEntry:
-        return cls(message.id, message.author.id, message.content, message.created_at, message.attachments)
+class MessageEntry(Storable):
 
     def __init__(self, messageID: int, authorID: int, content: str, timestamp: datetime, attachments: List[Attachment] = list()) -> None:
         self._id: int = messageID
@@ -62,3 +62,43 @@ class MessageEntry():
     @property
     def attachments(self) -> List[AttachmentEntry]:
         return self._attachments
+        
+
+    @classmethod
+    def fromMessage(cls, message: Message) -> MessageEntry:
+        return cls(message.id, message.author.id, message.content, message.created_at, message.attachments)
+
+    @classmethod
+    def __table__(self) -> Table:
+        # create a table builder
+        t_builder: TableBuilder = TableBuilder()
+        # set the table's name
+        t_builder.setName('Messages')
+
+        # create a column builder
+        c_builder: ColumnBuilder = ColumnBuilder()
+        # build columns
+        t_builder.addColumn(c_builder.setName('ID').setType('INTEGER').isPrimary().isUnique().column())
+        t_builder.addColumn(c_builder.setName('AuthorID').setType('INTEGER').column())
+        t_builder.addColumn(c_builder.setName('Content').setType('TEXT').column())
+        t_builder.addColumn(c_builder.setName('Timestamp').setType('TIMESTAMP').column())
+        # build the table
+        table: Table = t_builder.table()
+        # return the table
+        return table
+
+    def __values__(self) -> Tuple[Any, ...]:
+        # create a tuple with the corresponding values
+        value: Tuple[Any, ...] = (self.id, self.author_id, self.content, self.timestamp)
+        # return the tuple
+        return value
+        
+    @classmethod
+    def __from_row__(cls: Type[TStorable], row: Row) -> TStorable:
+        # get the values from the row
+        id: int = row['ID']
+        author_id: int = row['AuthorID']
+        content: str = row['Content']
+        timestamp: datetime = row['Timestamp']
+        # return the Metadata
+        return MessageEntry(id, author_id, content, timestamp)
