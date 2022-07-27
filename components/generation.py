1fc707b4ceaafea54ee293889aad27c5faf10288
Nathan Latchaw <nlatchaw@drakenintl.com>
2022-07-27 14:52:46 -0500

    factored out data classes

--- components/generation.py
+++ components/generation.py
@@ -8,30 +8,13 @@ from typing import List, Optional, Pattern, Tuple, Union
 import discord
 import markovify
 import nltk
+from components.models.generation import ChannelUser, POSifiedText
 from context import Context
 from discord import ClientUser, Guild, Member, Message, TextChannel, User
 from providers.channelArchive import ChannelArchive
 from settings.settings import Settings
 
 nltk.download('averaged_perceptron_tagger', quiet=True)
-
-class POSifiedText(markovify.NewlineText):
-    def word_split(self, sentence: str):
-        words = re.split(self.word_split_pattern, sentence)
-        words = [ "::".join(tag) for tag in nltk.pos_tag(words) ]
-        return words
-
-    def word_join(self, words: str):
-        sentence = " ".join(word.split("::")[0] for word in words)
-        return sentence
-
-    
-class ChannelUser:
-    def __init__(self, guild: Guild, channel: TextChannel) -> None:
-        self.id: int = channel.id
-        self.name: str = channel.name
-        self.avatar_url: str = guild.icon_url
-        self.mention: str = channel.mention
 
 
 class Generation():
